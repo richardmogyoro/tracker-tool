a774e5f64ba932f223f85ba56c5162ca19990c23
richardmogyoro <richard.mogyoro@gmail.com>
2022-05-08 19:46:38 +0200

    add skeleton code from Initializr

---
+++ tracker-tool/src/main/java/com/example/trackertool/TrackerToolApplication.java
@@ -0,0 +1,13 @@
+package com.example.trackertool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackerToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackerToolApplication.class, args);
+	}
+
+}
---
+++ tracker-tool/src/test/java/com/example/trackertool/TrackerToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trackertool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackerToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
